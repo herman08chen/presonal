8b4442516ec0c4112f1dc0eec4c6f1e885a3bb73
Herman Chen <herman08chen@gmail.com>
2024-07-11 14:43:08 -0400

    worked the number game

--- 01-numgame/numgame.js
+++ 01-numgame/numgame.js
@@ -4,17 +4,26 @@
 //  3. Put all other code inside functions
 const numField = document.getElementById("num-field");
 const messageText = document.getElementById("message-text");
+const numGuessesText = document.getElementById("num-guesses-text");
 
 let secret;
 let min = 1;
 let max = 100;
+let numGuesses = 0;
 
 var myConfetti = confetti.create(null, {
     resize: true,
     useWorker: true
 });
 
+function reset(){
+    numGuesses = 0;
+    loadGame();
+}
+
 function loadGame(){
+    numGuessesText.innerHTML = `Number of Guesses: <b>${numGuesses}</b>`; 
+    messageText.innerHTML = '';
     numField.min = min;
     numField.max = max;
     numField.value = max;
@@ -25,19 +34,22 @@ function loadGame(){
 }
 
 function makeGuess(){
+    numGuesses++;
+    numGuessesText.innerHTML = `Number of Guesses: <b>${numGuesses}</b>`; 
+
     let guess = parseInt(numField.value);
     console.log(`Guess: ${guess}`);
     if(guess < secret){
-        messageText.innerHTML = `${guess} is too low`;
+        messageText.innerHTML += `${guess} is too low<br>`;
     }
     else if(guess > secret){
-        messageText.innerHTML = `${guess} is too high`;
+        messageText.innerHTML += `${guess} is too high<br>`;
     }
     else if(guess == secret){
-        messageText.innerHTML = `${guess} is correct!`;
+        messageText.innerHTML += `${guess} is correct!<br>`;
         myConfetti({particleCount: 500, spread: 160});
     }
     else{
-        messageText.innerHTML = `Invalid Guess`;
+        messageText.innerHTML += `Invalid Guess<br>`;
     }
 }
