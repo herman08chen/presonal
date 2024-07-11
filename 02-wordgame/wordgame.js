7f686df821a6acefabe6b1504593ebbd673af7ca
Herman Chen <herman08chen@gmail.com>
2024-07-11 15:35:50 -0400

    add rand color

---
+++ 02-wordgame/wordgame.js
@@ -0,0 +1,44 @@
+let json;
+const body = document.body;
+
+// Returns a random integer between min and max
+//   [min, min+1, min+2, ... , max-1, max]
+function randInt(min, max) {
+    let rand = Math.random();
+    rand = rand * (max - min + 1);
+    rand = rand + min;
+    rand = Math.floor(rand);
+    return rand;
+}
+
+function loadGame() {
+    fetch('./words_dictionary.json')
+        .then(response => response.text())
+        .then(text => {
+            // Split the text by lines to get individual words
+            json = JSON.parse(text);
+            console.log('Words loaded!');
+            wordsLoaded();
+        })
+        .catch(error => {
+            console.error('Error fetching words: ', error);
+        });
+        randomBackgroundColor();
+}
+
+function randomBackgroundColor(){
+    let random = randInt(0, 359);
+    let colorString = `hsl(${random}, 80%, 90%)`;
+    body.style.backgroundColor = colorString;
+}
+
+
+// TODO: write function isWord(word)
+
+// For checking word:  json.hasOwnProperty("programming")
+// For array of words: let arr = Object.keys(json)
+// For a random word:  let word = arr[randInt(0, arr.length - 1)];
+
+function wordsLoaded(){
+    
+}
